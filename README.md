f79d3459ee11684bdd1c0a0f0e2b18a2937c8106
Gustavo Almeida <gualmeida2004@gmail.com>
2023-08-30 14:54:31 -0300

    Configuração inicial do ambiente

---
+++ README.md
@@ -0,0 +1,30 @@
+<!-- TODO: Banner deve vir aqui -->
+
+<!-- TODO: Badges devem vir aqui -->
+
+# 💲Mais Finanças App💲
+
+O Mais Finanças App é um aplicativo de gerenciamento de despesas e auxílio para aquisição de objetivos financeiros desenvolvido para a plataforma Android.
+
+## 📋 Funcionalidades
+
+- [ ] Registro de Despesas
+- [ ] Acompanhamento de Gastos
+- [ ] Lembretes e Notificações
+- [ ] Objetivos Financeiros
+- [ ] Sugestões para balanceamento de gastos
+- [ ] Autenticação e autorização
+- [ ] Caching dos dados
+- [ ] Criptografia dos dados sensíveis
+
+## 📱Screenshots
+
+![Protótipos](./assets/Protótipos.png)
+
+## ⇩ Instalação
+
+O download do arquivo APK mais recente do aplicativo está disponível através deste [link](https://github.com/mais-financas/mais-financas-app/tree/main/app/release/app-release.apk?raw=true).
+
+## 🫴Autores
+
+- [Gustavo Almeida Carvalho](https://www.github.com/gustxvo)
---
+++ app/src/main/java/com/neuralnet/maisfinancas/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.neuralnet.maisfinancas
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.neuralnet.maisfinancas.ui.theme.MaisFinancasTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaisFinancasApp()
+        }
+    }
+}
+
+@Composable
+fun MaisFinancasApp() {
+    MaisFinancasTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Text("Mais Finanças")
+        }
+    }
+}
